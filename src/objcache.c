83c9e88b1571fd6c073368a91805e6cec9e8e7bf
aatbip <anantabipal2010@gmail.com>
2025-11-18 10:18:49 +0545

    Add allocator function and freelist link

--- src/objcache.c
+++ src/objcache.c
@@ -4,11 +4,16 @@
 
 #define PAGE_SIZE 4 * 1024 // 4K
 
-typedef struct obj_slabctl {
+typedef struct objc_bufctl {
+  void *next;
+} objc_bufctl_t;
+
+typedef struct objc_slabctl {
   int ref_count;
-  struct obj_slabctl *next;
-  struct obj_slabctl *prev;
-} obj_slabctl_t;
+  objc_bufctl_t *freebuf; // head of the freebuf
+  struct objc_slabctl *next;
+  struct objc_slabctl *prev;
+} objc_slabctl_t;
 
 typedef struct objc_cache {
   char *name;
@@ -16,11 +21,10 @@ typedef struct objc_cache {
   int align;
   constructor c;
   destructor d;
-  obj_slabctl_t *free_slab; // partial or complete slab
+  objc_slabctl_t *free_slab; // partial or complete slab
 } objc_cache_t;
 
-objc_cache_t *objc_cache_create(char *name, size_t size, int align,
-                                constructor c, destructor d) {
+objc_cache_t *objc_cache_create(char *name, size_t size, int align, constructor c, destructor d) {
   objc_cache_t *cache = (objc_cache_t *)malloc(sizeof(*cache));
   cache->name = name;
   cache->size = size;
@@ -30,4 +34,57 @@ objc_cache_t *objc_cache_create(char *name, size_t size, int align,
   cache->free_slab = NULL; // slab is not created yet
 
   return cache;
+}
+
+void *objc_cache_alloc(objc_cache_t *cache) {
+  // printf("page size: %d\nobjc_slabctl_t: %zu\ncache size: %zu \n
+  // objc_bufctl_t: %zu\n",
+  //        PAGE_SIZE, sizeof(objc_slabctl_t), cache->size,
+  //        sizeof(objc_bufctl_t));
+
+  void *slab = cache->free_slab;
+
+  unsigned short buffer_size = cache->size + sizeof(objc_bufctl_t);
+  // printf("buf size: %d\n", buffer_size);
+
+  /*total number of buffers that can be allocated*/
+  unsigned short total_buf = (PAGE_SIZE - sizeof(objc_slabctl_t)) / (buffer_size);
+  // printf("total buf: %d\n", total_buf);
+
+  if (!slab) {
+    /* If a free slab is not available or a slab is not yet created
+     * then we create a new empty slab of PAGE_SIZE.*/
+    slab = malloc(PAGE_SIZE);
+
+    /*fragmented bytes*/
+    // unsigned short unused =
+    //     (PAGE_SIZE - sizeof(objc_slabctl_t)) % (buffer_size);
+
+    for (char *p = slab; p < (char *)slab + total_buf * buffer_size; p += buffer_size) {
+
+      objc_bufctl_t *bufctl = (objc_bufctl_t *)(p + cache->size);
+      char *next_buf = p + buffer_size;
+      if (next_buf >= (char *)slab + total_buf * buffer_size) {
+        bufctl->next = NULL;
+      } else {
+        bufctl->next = (objc_bufctl_t *)next_buf;
+      }
+    }
+    objc_slabctl_t *slabctl = (objc_slabctl_t *)((char *)slab + (total_buf * buffer_size) - sizeof(objc_slabctl_t));
+
+    slabctl->freebuf = ((objc_bufctl_t *)(slab + cache->size))->next;
+    cache->free_slab = slab;
+
+    cache->c(slab, cache->size);
+
+    return slab;
+  }
+  objc_slabctl_t *slabctl = (objc_slabctl_t *)((char *)slab + (total_buf * buffer_size) - sizeof(objc_slabctl_t));
+
+  void *cur_freebuf = slabctl->freebuf;
+
+  slabctl->freebuf = ((objc_bufctl_t *)(cur_freebuf))->next;
+  void *cur_freeslab = cur_freebuf - cache->size;
+  cache->c(cur_freeslab, cache->size);
+  return cur_freeslab; // current free slab
 }
--- test/test.c
+++ test/test.c
@@ -1,8 +1,22 @@
 #include "objcache.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct test {
+  int x, y;
+} test_t;
+
+void c(void *p, size_t size) {
+  test_t *q = p;
+  q->x = 33;
+  q->y = 22;
+}
 
 int main(void) {
-
-  //
+  objc_cache_t *cache = objc_cache_create("rand", sizeof(test_t), 0, c, NULL);
+  test_t *p = (test_t*)objc_cache_alloc(cache);
+  printf("x: %d\n y: %d\n", p->x, p->y);
+  test_t *q = (test_t*)objc_cache_alloc(cache);
+  printf("x1: %d\n y1: %d\n", q->x, q->y);
   return 0;
 }
